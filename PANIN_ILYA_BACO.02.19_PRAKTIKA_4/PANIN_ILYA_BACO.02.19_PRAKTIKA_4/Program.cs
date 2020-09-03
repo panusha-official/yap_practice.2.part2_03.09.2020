cb58156e46935a3465e605bd227f0cd9075a8573
PaNuSHa <70700507+panusha-official@users.noreply.github.com>
2020-09-03 17:40:25 +0300

    Add files via upload

---
+++ PANIN_ILYA_BACO.02.19_PRAKTIKA_4/PANIN_ILYA_BACO.02.19_PRAKTIKA_4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace PANIN_ILYA_BACO._02._19_PRAKTIKA_4
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string inFilename = @"c:\afd.txt";
+      string outFilename = @"c:\afd.txt.gz";
+
+
+      string inFilename1 = @"c:\afdd.txt";
+      FileStream sourceFile = File.OpenRead(outFilename);
+      FileStream destFile = File.Create(inFilename1);
+      GZipStream compStream =
+      new GZipStream(sourceFile, CompressionMode.Decompress);
+      int theByte = compStream.ReadByte();
+      while (theByte != -1)
+      {
+        destFile.WriteByte((byte)theByte);
+        theByte = compStream.ReadByte();
+      }
+
+      compStream.Close();
+    }
+  }
+}
---
+++ PANIN_ILYA_BACO.02.19_PRAKTIKA_5/PANIN_ILYA_BACO.02.19_PRAKTIKA_5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.IO.IsolatedStorage;
+
+namespace PANIN_ILYA_BACO._02._19_PRAKTIKA_5
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      IsolatedStorageFile userStore = IsolatedStorageFile.GetUserStoreForAssembly();
+      IsolatedStorageFileStream userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Create, userStore);
+      StreamWriter userWriter = new StreamWriter(userStream); 
+      userWriter.WriteLine("User Prefs"); 
+      userWriter.Close();
+
+      string[] files = userStore.GetFileNames("UserSettings.set");
+      if (files.Length == 0)
+      {
+        userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Open, userStore); 
+        StreamReader userReader = new StreamReader(userStream); 
+        string contents = userReader.ReadToEnd();
+      }
+    }
+  }
+}
